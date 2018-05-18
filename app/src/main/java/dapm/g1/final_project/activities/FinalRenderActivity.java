fb2769c0d9d4b7ac780304778623a08f85ea8e3b
houdayec <houdayer.corentin@gmail.com>
2018-05-18 17:40:01 +0200

    Merge started + need to display the final anamorphosis in ImageView

--- app/src/main/java/dapm/g1/final_project/activities/FinalRenderActivity.java
+++ app/src/main/java/dapm/g1/final_project/activities/FinalRenderActivity.java
@@ -1,25 +1,41 @@
 package dapm.g1.final_project.activities;
 
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import dapm.g1.final_project.MainActivity;
+import dapm.g1.final_project.PathUtil;
 import dapm.g1.final_project.R;
+import dapm.g1.final_project.VideoUtils;
+import wseemann.media.FFmpegMediaMetadataRetriever;
 
 public class FinalRenderActivity extends AppCompatActivity {
 
     /**
      * Binding view elements
      */
+    @BindView(R.id.imageViewAnamorphosis)
+    ImageView mImageViewAnamorphosis;
+
     @BindView(R.id.downloadAnamorphosisButton)
     ImageButton mDownloadAnamorphosisButton;
 
@@ -27,8 +43,23 @@ public class FinalRenderActivity extends AppCompatActivity {
     ImageButton mShareAnamorphosisButton;
 
     @BindView(R.id.backToMenuButton)
-    ImageButton mBackToMenuButton;
+    Button mBackToMenuButton;
 
+    private String duration;
+    protected LinearLayout llay;
+    protected ProgressDialog effectProgressDialog;
+    public FFmpegMediaMetadataRetriever mediaMetadataRetriever;
+    public double intervalRefresh;
+    // protected List<Bitmap> listFrames = new ArrayList<>();
+    protected  Bitmap finalBmp;
+    protected ProgressDialog mProgressDialog;
+    public String fileManager;
+    private int sample = 1;
+    private Uri uriData;
+
+    int scale=1;
+
+    @SuppressLint("NewApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +67,17 @@ public class FinalRenderActivity extends AppCompatActivity {
 
         // Binding the view
         ButterKnife.bind(this);
+
+
+
+        uriData = Uri.parse(getIntent().getStringExtra("uri_video"));
+        fileManager = PathUtil.getPath(this, uriData);
+
+        mediaMetadataRetriever = new FFmpegMediaMetadataRetriever();
+        mediaMetadataRetriever.setDataSource(fileManager);
+
+        // Starting anamorphosis
+        new FramesExtraction().execute();
 
     }
 
@@ -58,4 +100,86 @@ public class FinalRenderActivity extends AppCompatActivity {
         Intent goToMainMenuIntent = new Intent(this, MainActivity.class);
         startActivity(goToMainMenuIntent);
     }
+
+    /**
+     * Inner class (Asynchronous task) to get all the frames from a video //TODO MICKAEL ALGO IMP ?
+     */
+    @SuppressLint("NewApi")
+    private class FramesExtraction extends AsyncTask<String, int[], String> {
+
+        int [] pixelStart=null;
+        Bitmap bmpStart=null;
+
+        @Override
+        protected String doInBackground(String... params) {int currentTime = 0;
+            Bitmap bmpStart=null;
+            int [] pixelStart=null;
+            int indice=0;
+            duration = mediaMetadataRetriever.extractMetadata(FFmpegMediaMetadataRetriever.METADATA_KEY_DURATION);
+            duration = String.valueOf(Integer.valueOf(duration) * 1000);
+            while(currentTime < Integer.valueOf(duration)){
+                Bitmap bmFrame = mediaMetadataRetriever.getFrameAtTime(currentTime,FFmpegMediaMetadataRetriever.OPTION_CLOSEST); //unit in microsecond
+                if(currentTime==0)
+                {
+                    bmpStart=bmFrame;
+                    pixelStart=new int[bmpStart.getWidth()*bmpStart.getHeight()];
+                    //bmpStart.getPixels(pixelStart, 0, bmpStart.getWidth(), 0, 0,bmpStart.getWidth(),bmpStart.getHeight());
+
+                }
+                if(bmFrame!=null)
+                {
+                    //testMaxime(pixelStart,bmFrame,indice);
+                    indice+=scale;
+                    int [] valeur={bmFrame.getWidth(),bmFrame.getHeight()};
+                    publishProgress(pixelStart,valeur);
+                }
+                //listFrames.add(bmFrame);
+                System.out.println("coucou");
+                Log.e("yo","yo");
+                currentTime += intervalRefresh;
+                System.out.println(currentTime);
+            }
+            if(bmpStart !=null) {
+                Bitmap finalBmp=Bitmap.createBitmap(bmpStart.getWidth(), bmpStart.getHeight(), Bitmap.Config.ARGB_8888);
+                finalBmp.setPixels(pixelStart, 0, bmpStart.getWidth(), 0, 0, bmpStart.getWidth(), bmpStart.getHeight());
+
+                //listFrames.add(finalBmp);
+            }
+            return "Executed";}
+
+
+
+        @Override
+        protected void onPostExecute(String result) {
+
+            //mProgressDialog.dismiss();
+
+           /* ImageView imgView=findViewById(R.id.imageView);
+            imgView.setImageBitmap(finalBmp);*/
+
+            Log.e("fin","fin");
+            Toast.makeText(FinalRenderActivity.this, "Traitement fini", Toast.LENGTH_SHORT).show();
+
+        }
+
+        @Override
+        protected void onPreExecute() {
+
+        }
+
+        @Override
+        protected void onProgressUpdate(int[]... values) {
+            int[] pixelStart=values[0];
+            int[] valeur=values[1];
+            int w=valeur[0];
+            int h=valeur[1];
+            Bitmap finalBmp=Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+            finalBmp.setPixels(pixelStart, 0, w, 0, 0,w, h);
+            mImageViewAnamorphosis.setImageBitmap(finalBmp);
+        }
+
+    }
+
+
+
 }
--- app/src/main/java/dapm/g1/final_project/activities/TypeActivity.java
+++ app/src/main/java/dapm/g1/final_project/activities/TypeActivity.java
@@ -43,16 +43,7 @@ public class TypeActivity extends AppCompatActivity {
 
     static final int REQUEST_VIDEO_CAPTURE = 1;
 
-    private String duration;
-    private int sample=1;
-    protected LinearLayout llay;
-    protected ProgressDialog effectProgressDialog;
-    public FFmpegMediaMetadataRetriever mediaMetadataRetriever;
-    public double intervalRefresh;
-   // protected List<Bitmap> listFrames = new ArrayList<>();
-    protected  Bitmap finalBmp;
-    protected ProgressDialog mProgressDialog;
-    public String fileManager;
+    private Uri uriData;
 
 
 
@@ -64,26 +55,8 @@ public class TypeActivity extends AppCompatActivity {
         // Binding view
         ButterKnife.bind(this);
 
-        // Setting up the view
-        mProgressDialog = new ProgressDialog(getApplicationContext());
+        uriData = Uri.parse(getIntent().getStringExtra("uri_video"));
 
-        // Starting async task (another thread)
-        new FramesExtraction().execute();
-
-    }
-
-    public void createAnamorphose(Bitmap currentBmp,int[] pixels,int index){
-        Log.e("coucouc","coucocucou");
-        int height=currentBmp.getHeight();
-        int width=currentBmp.getWidth();
-        int currentPixel[]=new int[height*width];
-        currentBmp.getPixels(currentPixel, 0, width, 0, 0, width, height);
-        if(index<height) {//if choose TOP
-            for (int j = 0; j < sample * width; j++) {
-                if(index*width+j<width*height)
-                    pixels[index * width + j] = currentPixel[index * width + j];
-            }
-        }
     }
 
     /**
@@ -91,134 +64,13 @@ public class TypeActivity extends AppCompatActivity {
      */
     @OnClick(R.id.generateAnamorphosis)
     void generateAnamorphosis(){
-        new FramesExtraction().execute();
+        Intent intentFinalRender = new Intent(this, FinalRenderActivity.class);
+        Bundle bundleArgs = new Bundle();
+        bundleArgs.putString("uri_video", uriData.toString());
+        intentFinalRender.putExtras(bundleArgs);
+        startActivity(intentFinalRender);
     }
 
-    /**
-     * Inner class (Asynchronous task) to get all the frames from a video //TODO MICKAEL ALGO IMP ?
-     */
-    private class FramesExtraction extends AsyncTask<String, Void, String> {
-
-        @Override
-        protected String doInBackground(String... params) {
-
-            Uri uriData = Uri.parse(getIntent().getStringExtra("uri_video"));
-            fileManager = PathUtil.getPath(getApplicationContext(), uriData);
-
-            int height = 0;
-            int width = 0;
-            System.out.println(fileManager);
-            //Log.e("path", pathSelectedVideo);
-            if (fileManager != null) {
-                //TODO
-                mediaMetadataRetriever = new FFmpegMediaMetadataRetriever();
-                mediaMetadataRetriever.setDataSource(fileManager);
-
-                duration = mediaMetadataRetriever.extractMetadata(FFmpegMediaMetadataRetriever.METADATA_KEY_DURATION);
-                Log.e("duration", duration);
-
-                // Create a Linear Layout for each contact?
-                llay = new LinearLayout(getApplicationContext());
-                llay.setOrientation(LinearLayout.HORIZONTAL);
-
-                int numberFrames = VideoUtils.getFrameRateVideo(fileManager);
-                System.out.println("The video has a " + numberFrames + " frames / second");
-
-                intervalRefresh = 1000000/numberFrames;
-
-
-                Bitmap bmFirst = mediaMetadataRetriever.getFrameAtTime(0,FFmpegMediaMetadataRetriever.OPTION_CLOSEST);
-                height=bmFirst.getHeight();
-                width=bmFirst.getWidth();
-
-                int size=height;
-                sample = size/(Integer.valueOf(duration)*numberFrames);
-
-
-                mProgressDialog.setTitle("test");
-                mProgressDialog.setMessage("test");
-                mProgressDialog.setIndeterminate(false);
-                //mProgressDialog.show();
-            }
-
-            int currentTime = 0;
-            System.out.println("duration " + duration);
-
-            final int[] pixelsFinal=new int[width*height];
-            int index=0;
-
-
-            if(duration != null){
-                duration = String.valueOf(Integer.valueOf(duration) * 1000);
-                System.out.println("duration : " + duration);
-                System.out.println("duration integer " + Integer.valueOf(duration));
-                System.out.println("Interval refresh " + intervalRefresh);
-                while(currentTime < Integer.valueOf(duration)){
-                    final int finalCurrentTime = currentTime;
-                    final  int finalIndex = index;
-                    new Thread(new Runnable() {
-                        @Override
-                        public void run() {
-                            if(mediaMetadataRetriever.getFrameAtTime(finalCurrentTime) != null){
-                                Bitmap bmFrame = mediaMetadataRetriever.getFrameAtTime(finalCurrentTime,FFmpegMediaMetadataRetriever.OPTION_CLOSEST); //unit in microsecond
-                                if(bmFrame!=null)
-                                {
-                                    createAnamorphose(bmFrame,pixelsFinal,finalIndex);
-                                    //index+=sample;
-                                   // int [] valeur={bmFrame.getWidth(),bmFrame.getHeight()};
-                                   // publishProgress(pixelsFinal,valeur);
-                                }
-                            }
-                        }
-                    }).start();
-                    index += sample;
-                    currentTime = finalCurrentTime;
-                    currentTime += intervalRefresh;
-                    System.out.println(currentTime);
-                }
-
-                finalBmp=Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
-                finalBmp.setPixels(pixelsFinal, 0, width, 0, 0, width, height);
-
-                //listFrames.add(finalBmp);
-
-            }
-
-
-
-            return "Executed";
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-
-            //mProgressDialog.dismiss();
-
-           /* ImageView imgView=findViewById(R.id.imageView);
-            imgView.setImageBitmap(finalBmp);*/
-
-            Log.e("fin","fin");
-            Toast.makeText(TypeActivity.this, "Traitement fini", Toast.LENGTH_SHORT).show();
-
-        }
-
-        @Override
-        protected void onPreExecute() {
-
-        }
-
-        @Override
-        protected void onProgressUpdate(Void... values) {
-           /* int[] pixelStart=values[0];
-            int[] valeur=values[1];
-            int w=valeur[0];
-            int h=valeur[1];
-            Bitmap finalBmp=Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
-            finalBmp.setPixels(pixelStart, 0, w, 0, 0,w, h);
-            ImageView imgView=findViewById(R.id.imageView);
-            imgView.setImageBitmap(finalBmp);*/
-        }
-    }
 
 
 }
