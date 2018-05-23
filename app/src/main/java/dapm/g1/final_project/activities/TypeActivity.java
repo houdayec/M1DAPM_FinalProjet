a76024bc1915796667fa20b01d1d2a93c32d616e
houdayec <houdayer.corentin@gmail.com>
2018-05-23 11:31:58 +0200

    added drawing  view + UI changes

--- app/src/main/java/dapm/g1/final_project/activities/TypeActivity.java
+++ app/src/main/java/dapm/g1/final_project/activities/TypeActivity.java
@@ -1,8 +1,14 @@
 package dapm.g1.final_project.activities;
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Point;
 import android.media.MediaExtractor;
 import android.media.MediaFormat;
 import android.net.Uri;
@@ -14,7 +20,9 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -33,19 +41,39 @@ import dapm.g1.final_project.R;
 import dapm.g1.final_project.VideoUtils;
 import wseemann.media.FFmpegMediaMetadataRetriever;
 
-public class TypeActivity extends AppCompatActivity {
+public class TypeActivity extends AppCompatActivity implements View.OnClickListener {
 
     /**
      * INTERN VARIABLES
      */
     @BindView(R.id.generateAnamorphosis)
-    ImageButton mGenerateAnamorphosisButton;
+    Button mGenerateAnamorphosisButton;
 
-    static final int REQUEST_VIDEO_CAPTURE = 1;
+    @BindView(R.id.validVideoFP)
+    ImageButton cancelVideo;
 
+    @BindView(R.id.cancelVideoFP)
+    ImageButton validVideo;
+
+    private Paint mPaint;
+    private boolean didUserAlreadyDraw = false;
+
+    float tempDx, tempDy = 0;
+
+    private List<Point> listPoints = new ArrayList<>();
+
+    public int width;
+    public  int height;
+    private Bitmap mBitmap;
+    private Canvas mCanvas;
+    private Path mPath;
+    private Paint   mBitmapPaint;
+    Context context;
+    private Paint circlePaint;
+    private Path circlePath;
     private Uri uriData;
-
-
+    private DrawingView dv;
+    private LinearLayout layoutDrawingView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,6 +85,23 @@ public class TypeActivity extends AppCompatActivity {
 
         uriData = Uri.parse(getIntent().getStringExtra("uri_video"));
 
+        dv = new DrawingView(this);
+        dv.setBackgroundColor(getColor(R.color.white));
+
+        layoutDrawingView = findViewById(R.id.layoutDrawingView);
+        layoutDrawingView.addView(dv);
+
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setDither(true);
+        mPaint.setColor(Color.BLUE);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeJoin(Paint.Join.ROUND);
+        mPaint.setStrokeCap(Paint.Cap.ROUND);
+        mPaint.setStrokeWidth(10);
+
+        validVideo.setOnClickListener(this);
+        cancelVideo.setOnClickListener(this);
     }
 
     /**
@@ -64,11 +109,126 @@ public class TypeActivity extends AppCompatActivity {
      */
     @OnClick(R.id.generateAnamorphosis)
     void generateAnamorphosis(){
-        Intent intentFinalRender = new Intent(this, Test_FingerPaint.class);
+        Intent intentFinalRender = new Intent(this, FinalRenderActivity.class);
         Bundle bundleArgs = new Bundle();
         bundleArgs.putString("uri_video", uriData.toString());
         intentFinalRender.putExtras(bundleArgs);
         startActivity(intentFinalRender);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.validVideoFP:
+                Intent intentFinalRender = new Intent(this, FinalRenderActivity.class);
+                Bundle bundleArgs = new Bundle();
+                bundleArgs.putString("uri_video", uriData.toString());
+                intentFinalRender.putExtras(bundleArgs);
+                startActivity(intentFinalRender);
+                break;
+            case R.id.cancelVideoFP:
+                tempDx = 0;
+                mCanvas = new Canvas();
+                listPoints = new ArrayList<>();
+                break;
+        }
+    }
+
+    public class DrawingView extends View {
+
+        public DrawingView(Context c) {
+            super(c);
+            context=c;
+            mPath = new Path();
+            mBitmapPaint = new Paint(Paint.DITHER_FLAG);
+            circlePaint = new Paint();
+            circlePath = new Path();
+            circlePaint.setAntiAlias(true);
+            circlePaint.setColor(Color.BLUE);
+            circlePaint.setStyle(Paint.Style.STROKE);
+            circlePaint.setStrokeJoin(Paint.Join.MITER);
+            circlePaint.setStrokeWidth(10);
+        }
+
+        @Override
+        protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+            super.onSizeChanged(w, h, oldw, oldh);
+
+            mBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+            mCanvas = new Canvas(mBitmap);
+
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            super.onDraw(canvas);
+
+            canvas.drawBitmap( mBitmap, 0, 0, mBitmapPaint);
+            canvas.drawPath( mPath,  mPaint);
+            canvas.drawPath( circlePath,  circlePaint);
+
+        }
+
+        private float mX, mY;
+        private static final float TOUCH_TOLERANCE = 4;
+
+        private void touch_start(float x, float y) {
+            mPath.reset();
+            mPath.moveTo(x, y);
+            mX = x;
+            mY = y;
+            listPoints.add(new Point((int)x, (int)y));
+        }
+
+        private void touch_move(float x, float y) {
+            float dx = Math.abs(x - mX);
+            float dy = Math.abs(y - mY);
+            System.out.println("coords : " + dx + " : " + dy);
+            System.out.println("coords : " + x + " : " + y);
+            if(tempDx < x){
+                if (dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE) {
+                    mPath.quadTo(mX, mY, (x + mX)/2, (y + mY)/2);
+                    mX = x;
+                    mY = y;
+                    circlePath.reset();
+                    circlePath.addCircle(mX, mY, 30, Path.Direction.CW);
+                    tempDx = x;
+                    listPoints.add(new Point((int)x, (int)y));
+                }
+            }
+
+        }
+
+        private void touch_up() {
+            mPath.lineTo(mX, mY);
+            circlePath.reset();
+            // commit the path to our offscreen
+            mCanvas.drawPath(mPath,  mPaint);
+            // kill this so we don't double draw
+            mPath.reset();
+        }
+
+        @Override
+        public boolean onTouchEvent(MotionEvent event) {
+            float x = event.getX();
+            float y = event.getY();
+
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    touch_start(x, y);
+                    invalidate();
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    touch_move(x, y);
+                    invalidate();
+                    break;
+                case MotionEvent.ACTION_UP:
+                    touch_up();
+                    invalidate();
+                    break;
+            }
+            return true;
+        }
     }
 
 
