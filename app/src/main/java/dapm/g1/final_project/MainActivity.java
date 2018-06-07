fe69a1e4c61b1228a24fee1d331afa742ab67dd8
houdayec <houdayer.corentin@gmail.com>
2018-06-07 11:25:49 +0200

    merging Interpolation algo

--- app/src/main/java/dapm/g1/final_project/MainActivity.java
+++ app/src/main/java/dapm/g1/final_project/MainActivity.java
@@ -11,6 +11,7 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -184,6 +185,7 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
     }
+
 
 
 }
