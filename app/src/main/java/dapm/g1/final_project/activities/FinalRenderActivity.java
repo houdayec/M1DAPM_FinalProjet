44cdc11d440263f5496dae17df8201d7ef3382d6
houdayec <houdayer.corentin@gmail.com>
2018-06-07 16:08:49 +0200

    UI changes

--- app/src/main/java/dapm/g1/final_project/activities/FinalRenderActivity.java
+++ app/src/main/java/dapm/g1/final_project/activities/FinalRenderActivity.java
@@ -130,6 +130,8 @@ public class FinalRenderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.activity_final_render);
+
         // Binding view
         ButterKnife.bind(this);
 
